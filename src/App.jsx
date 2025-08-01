51eb1a2fd3f49d1b53b56dcc1c6db2b4acedab69
Zoraliuky <3158637286@qq.com>
2025-08-01 23:02:03 +0800

    chore(ui)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-    import React from 'react';
-    import { Routes, Route } from 'react-router-dom';
-    import Navbar from './components/Navbar'; //  导入 Navbar 组件
-    import BlindBoxList from './pages/BlindBoxList';
-    import BlindBoxDetail from './pages/BlindBoxDetail';
-    import LoginPage from './pages/LoginPage';
-    import RegisterPage from './pages/RegisterPage';
-    import MyOrdersPage from './pages/MyOrdersPage'; // 导入新页面
-    function App() {
-      return (
-        <div>
-          <Navbar /> {/* 2. 将 Navbar 放在 Routes 的上方 */}
-          <main>
-            <Routes>
-              <Route path="/" element={<BlindBoxList />} />
-              <Route path="/box/:id" element={<BlindBoxDetail />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/my-orders" element={<MyOrdersPage />} /> {/* 添加新路由 */}
-            </Routes>
-          </main>
-        </div>
-      );
-    }
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-    export default App;
-    
+// 1. 从 @mui/material 导入 ThemeProvider, createTheme 和 CssBaseline
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// 导入我们所有的组件和页面
+import Navbar from './components/Navbar';
+import BlindBoxList from './pages/BlindBoxList';
+import BlindBoxDetail from './pages/BlindBoxDetail';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import MyOrdersPage from './pages/MyOrdersPage';
+import AdminPage from './pages/AdminPage';
+import ProtectedRoute from './components/ProtectedRoute';
+
+// 2. 创建一个默认的 MUI 主题
+const theme = createTheme();
+
+function App() {
+  return (
+    // 3. 用 ThemeProvider 包裹所有内容，并将我们创建的 theme 传入
+    <ThemeProvider theme={theme}>
+      {/* 4. CssBaseline 组件必须放在 ThemeProvider 之后，它会进行样式重置 */}
+      <CssBaseline />
+      
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            {/* 公共路由 */}
+            <Route path="/" element={<BlindBoxList />} />
+            <Route path="/box/:id" element={<BlindBoxDetail />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/my-orders" element={<MyOrdersPage />} />
+
+            {/* 管理员专属的受保护路由 */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/admin" element={<AdminPage />} />
+            </Route>
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
